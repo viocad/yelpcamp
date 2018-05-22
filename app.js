e560614097afd49ec007d6dbd69723e819c74172
Zoe Wong <zoew1013@gmail.com>
2018-05-22 05:23:42 +0000

    add connection to mlab

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ var campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes          = require("./routes/index");
 
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://vio:pudding@ds135552.mlab.com:35552/cadyelpcamp");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
